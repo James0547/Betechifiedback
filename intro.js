5e70114a181a7b1c6a76746e8f080d710d971a9c
HeroHero <jamesquaicoeever@gmail.com>
2025-10-17 01:26:00 +0000

    My first Betecfied assignment

---
+++ intro.js
@@ -0,0 +1,20 @@
+// BETECHIFIED ASSINGNMENT ON 06/10/2025 WEEK ONE(1)
+
+// A CONSOLE PROGRAM  TO SHOW NAME, AGE, PROGRAMMING LANGUAGE OF YOUR CHOICE
+
+console.log("BETECHIFIED, A CONSOLE PROGRAM  TO SHOW NAME, AGE, PROGRAMMING LANGUAGE OF YOUR CHOICE !");
+
+// let name = "James Quaicoe";
+// let age = 24;
+// let programming_language = "Python";
+// console.log(name, age, programming_language);
+
+
+function betechified(name, age, programming_language){
+    return name, age, programming_language;
+
+
+}
+
+console.log("Name: James Quaicoe", 24, "programming_language: Python", betechified);
+
